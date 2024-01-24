004f6f03550b7cab62d7e9f01d8dad76af734b92
christopher.aguilars@outlook.com <christopher.aguilars@outlook.com>
2024-01-24 07:46:20 -0500

    traslado de contenido a configuracion

--- routes/web.php
+++ routes/web.php
@@ -29,29 +29,34 @@ Route::middleware(['auth'])->group(function () {
     Route::prefix('/configuracion')->group(function () {
         Route::prefix('/financiero')->group(function () {
             Route::get('/tipo-transaccion', function () {
-                return view('livewire.financiero.tipo-transaccion.index');
+                return view('livewire.configuracion.financiero.tipo-transaccion.index');
             })->name('tipo-transaccion');
             Route::get('/generica', function () {
-                return view('livewire.financiero.generica.index');
+                return view('livewire.configuracion.financiero.generica.index');
             })->name('generica');
             Route::get('/sub-generica-nivel-1', function () {
-                return view('livewire.financiero.sub-generica-nivel-1.index');
+                return view('livewire.configuracion.financiero.sub-generica-nivel-1.index');
             })->name('sub-generica-nivel-1');
             Route::get('/sub-generica-nivel-2', function () {
-                return view('livewire.financiero.sub-generica-nivel-2.index');
+                return view('livewire.configuracion.financiero.sub-generica-nivel-2.index');
             })->name('sub-generica-nivel-2');
             Route::get('/especifica-nivel-1', function () {
-                return view('livewire.financiero.especifica-nivel-1.index');
+                return view('livewire.configuracion.financiero.especifica-nivel-1.index');
             })->name('especifica-nivel-1');
             Route::get('/especifica-nivel-2', function () {
-                return view('livewire.financiero.especifica-nivel-2.index');
+                return view('livewire.configuracion.financiero.especifica-nivel-2.index');
             })->name('especifica-nivel-2');
         });
         Route::prefix('/contabilidad')->group(function () {
             Route::get('/cuentas', function () {
-                return view('livewire.contable.cuentas.index');
+                return view('livewire.configuracion.contable.cuentas.index');
             })->name('cuentas');
         });
+    });
+    Route::prefix('/contable')->group(function () {
+        Route::get('/caja-bancos', function () {
+            return view('livewire.contable.caja-bancos.index');
+        })->name('caja-bancos');
     });
 });
 
